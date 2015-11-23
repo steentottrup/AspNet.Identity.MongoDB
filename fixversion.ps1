7e12d3aa404b7dc697519d1c1a996f00640c205a
steentottrup <sft@creativeminds.dk>
2015-11-23 15:06:22 +0100

    fixing root in powershell script

--- fixversion.ps1
+++ fixversion.ps1
@@ -1,4 +1,4 @@
-$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$root = (split-path -parent $MyInvocation.MyCommand.Definition)
 $version = [System.Reflection.Assembly]::LoadFile("$root\src\AspNet.Identity.MongoDB\bin\Release\AspNet.Identity.MongoDB.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
